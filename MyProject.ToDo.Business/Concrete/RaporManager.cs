fd4cef7c0ffe9d2be901d510c456ee4455d7c60d
anilates97 <55891642+anilates97@users.noreply.github.com>
2020-12-25 02:04:55 +0300

    updateRaporManager

--- MyProject.ToDo.Business/Concrete/RaporManager.cs
+++ MyProject.ToDo.Business/Concrete/RaporManager.cs
@@ -14,47 +14,47 @@ namespace MyProject.ToDo.Business.Concrete
         public RaporManager(IRaporDal raporDal)
         {
             _raporDal = raporDal;
-            // raporRepository = new EfRaporRepository(); // tehlikeli bir hareket yapıyoruz böyle tanımlayarak ama ilerde düzelteceğiz -> Edit Ve düzelttik sorunu. 
         }
 
-        public Rapor GetirGorevileId(int id)
-        {
-            return _raporDal.GetirGorevileId(id);
-        }
+            public Rapor GetirGorevileId(int id)
+            {
+                return _raporDal.GetirGorevileId(id);
+            }
 
-        public List<Rapor> GetirHepsi()
-        {
-            return _raporDal.GetirHepsi();
-        }
+            public List<Rapor> GetirHepsi()
+            {
+                return _raporDal.GetirHepsi();
+            }
 
-        public Rapor GetirIdile(int id)
-        {
-            return _raporDal.GetirIdile(id);
-        }
+            public Rapor GetirIdile(int id)
+            {
+                return _raporDal.GetirIdile(id);
+            }
 
-        public int GetirRaporSayisi()
-        {
-            return _raporDal.GetirRaporSayisi();
-        }
+            public int GetirRaporSayisi()
+            {
+                return _raporDal.GetirRaporSayisi();
+            }
 
-        public int GetirRaporSayisiileAppUserId(int id)
-        {
-            return _raporDal.GetirRaporSayisiileAppUserId(id);
-        }
+            public int GetirRaporSayisiileAppUserId(int id)
+            {
+                return _raporDal.GetirRaporSayisiileAppUserId(id);
+            }
 
-        public void Guncelle(Rapor tablo)
-        {
-            _raporDal.Guncelle(tablo);
-        }
+            public void Guncelle(Rapor tablo)
+            {
+                _raporDal.Guncelle(tablo);
+            }
 
-        public void Kaydet(Rapor tablo)
-        {
-            _raporDal.Kaydet(tablo);
-        }
+            public void Kaydet(Rapor tablo)
+            {
+                _raporDal.Kaydet(tablo);
+            }
 
-        public void Sil(Rapor tablo)
-        {
-            _raporDal.Sil(tablo);
+            public void Sil(Rapor tablo)
+            {
+                _raporDal.Sil(tablo);
+            }
         }
     }
-}
+
